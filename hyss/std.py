72f574b98bc2e6cc47f58e1daebf8ef3c377d83e
Charlie Paterson <Charlieed.paterson@googlemail.com>
2022-08-12 18:10:58 +0100

    Refactor: moved files around somewhat

---
+++ hyss/std.py
@@ -0,0 +1,37 @@
+from typing import Union, Literal
+
+
+def stylesheet(styles: dict[str, dict], minify: bool = True):
+    output = 
+    br = '' if minify else '
+'
+    for name, value in styles.items():
+        if type(value) is str:
+            output += f'{br}{name}:{value};'
+        else:
+            output += f'{br}{name}' + '{' + compile_css(value, minify=minify) + br + '}'
+
+    return output
+
+
+def sub_style(parent_class: str, styles: dict[str, any]):
+    return {f'{parent_class} {k}': v for k, v in styles.items()}
+
+
+def animation(
+        name: str,
+        targets: list[str],
+        keyframes: dict[str, any],
+        duration_ms: Union[int, float] = 0,
+        delay_ms: Union[int, float] = 0,
+        play_state: Literal['paused', 'running', 'initial', 'inherit'] = 'running',
+        timing_fn: str = 'ease',
+        direction: Literal['normal', 'reverse', 'alternate', 'alternate-reverse', 'initial', 'inherit'] = 'normal',
+        iteration_count: Union[int, Literal['infinite', 'initial', 'inherit']] = 1,
+        fill_mode: Literal['none', 'forwards', 'backwards', 'both', 'initial', 'inherit'] = 'none',
+):
+    props = f'{name} {duration_ms}ms {timing_fn} {delay_ms}ms {iteration_count} {direction} {fill_mode} {play_state}'
+    return {
+        f'@keyframes {name}': keyframes,
+        **{target: {'animation': props} for target in targets}
+    }
