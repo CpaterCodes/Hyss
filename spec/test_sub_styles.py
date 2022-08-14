3abc5bb66b932aaf506f0ec6f1cefa317f5d5858
Charlie Paterson <Charlieed.paterson@googlemail.com>
2022-08-14 15:23:26 +0100

    Test: Passing test to demonstrate workings of sub_style helper

---
+++ spec/test_sub_styles.py
@@ -0,0 +1,14 @@
+import pytest 
+from HYSS.hyss import sub_style, stylesheet
+
+def test_basic_substyle():
+    target = 'body{background-color:blue;}body div{color:yellow;}body p{color:white;}'
+    assert stylesheet({
+        'body': { 'background-color': 'blue'},
+        **sub_style('body', 
+            {
+                'div': {'color': 'yellow'},
+                'p': {'color':'white'}
+            }
+        ),
+        }) == target
