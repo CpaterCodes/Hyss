f868dc79fea6ed8bd4b44356335803005afb233b
Charlie Paterson <Charlieed.paterson@googlemail.com>
2022-08-17 13:10:57 +0100

    Test: Failing test for basic animation initialisation. May consider using a prefix, but this could pose design philosophy problems

---
+++ spec/test_animations.py
@@ -0,0 +1,23 @@
+import pytest
+from HYSS.hyss import stylesheet, animation
+
+def test_default():
+    style = stylesheet({
+        'div': {
+            'color': 'blue'
+            },
+        **animation(
+            'move',
+            ['div'],
+            {'from':{'color': 'black'}, 'to': {'color': 'blue'}}
+        )
+        })
+
+    intent = "div{color:blue;}@keyframes move{from{color:black;}to{color:blue;}div{animation:move 0ms ease 0ms 1 normal none running}}"
+    assert style == intent
+
+def test_animation():
+    pass
+
+def test_multiple_targets():
+    pass
