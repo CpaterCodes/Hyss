5b6396dc8f04ab4ed3a58dfc16bf0fadea85e3cb
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2024-01-09 14:07:31 +0000

    Refactor: Migration of sample CSS to external files underway to facilitate tests' role as a demonstration of how CSS is created

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -1,16 +1,16 @@
 import pytest
+from .helpers import css_from_file
 from ..hyss.format import with_linebreaks, with_indents
 
+
 def test_linebreaks():
-    before = 'div{color:white;justify-content:center;}'
-    after = 'div{\ncolor:white;\njustify-content:center;\n}\n'
-    
-    assert with_linebreaks(before) == after
+    minified_css = css_from_file('basic_sheet.css')
+    linebroken_css = css_from_file('linebroken_sheet.css')
+    assert with_linebreaks(minified_css) == linebroken_css 
 
 
 def test_indents():
-    before = 'div{\ncolor:white;\n}\np{\ncolor:black;\n}\n'
-    after = 'div{\n\tcolor:white;\n}\np{\n\tcolor:black;\n}\n'
-
-    assert with_indents(before) == after
+    linebroken_css = css_from_file('linebroken_sheet.css')
+    indented_css = css_from_file('indented_sheet.css')
+    assert with_indents(linebroken_css) == indented_css
 
--- tests/test_style_sheets.py
+++ tests/test_style_sheets.py
@@ -1,16 +1,17 @@
 import pytest
 from ..hyss import stylesheet
 
-def test_basic_stylesheet():
-    css = "display:flex;justify-content:center;" 
+def test_style_attributes():
+    css = 'display:flex;justify-content:center;'
     assert stylesheet(
             {'display': 'flex', 'justify-content': 'center'}
     ) == css
 
 
-def test_multiple_classes():
-
-    css = 'body{background-color:yellow;}div{display:flex;color:red;}' 
+def test_full_stylesheet():
+    
+    with open('tests/assets/basic_sheet.css', 'r') as basic_sheet:
+        css = basic_sheet.read().strip() 
     assert stylesheet({
         'body': {
             'background-color': 'yellow'
